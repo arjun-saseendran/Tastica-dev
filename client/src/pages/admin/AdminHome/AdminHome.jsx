b9d74402dd4a2e400a72b5e855a72a580571155e
arjun-saseendran <arjunsaseendran@icloud.com>
2025-05-22 16:14:08 +0530

    add monthly, yearly and weekly and sales performance

--- client/src/pages/admin/AdminHome/AdminHome.jsx
+++ client/src/pages/admin/AdminHome/AdminHome.jsx
@@ -1,9 +1,9 @@
-import { BarChart } from "../../../components/admin/BarChart/BarChart";
-import { DoughnutChart } from "../../../components/admin/DoughnutCart/DoughnutChart";
-import { ExpenseProfitChart } from "../../../components/admin/ExpenseProfitChart/ExpenseProfitChart";
+import { WeeklySales } from "../../../components/admin/WeeklySales/WeeklySales";
+import { CategorySales } from "../../../components/admin/CategorySales/CategorySales";
 import { MonthlySales } from "../../../components/admin/MonthlySales/MonthlySales";
 import { SalesPerformance } from "../../../components/admin/SalesPerformance/SalesPerformance";
 import { YearlySales } from "../../../components/admin/YearlySales/YearlySales";
+import { SalesChart } from "../../../components/admin/SalesChart/SalesChart";
 
 export const AdminHome = () => {
   return (
@@ -11,18 +11,18 @@ export const AdminHome = () => {
       <div className="lg:h-[700px] p-1">
         <section className="grid grid-cols-12 gap-2">
           <div className="col-span-12 lg:col-span-4">
-            <DoughnutChart />
+            <CategorySales />
           </div>
           <div className="col-span-12 lg:col-span-4">
             <SalesPerformance />
           </div>
           <div className="col-span-12 lg:col-span-4">
-            <ExpenseProfitChart />
+            <SalesChart />
           </div>
         </section>
         <section className="grid grid-cols-12 gap-2 mt-2">
           <div className="col-span-12 lg:col-span-4">
-            <BarChart />
+            <WeeklySales />
           </div>
           <div className="col-span-12 lg:col-span-4">
             <MonthlySales />
